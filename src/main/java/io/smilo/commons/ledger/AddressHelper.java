17aba549343b6d7ff98b48e398fc811284d8bc34
Cristiano Ruiz Mazzon <cmazzon@gmail.com>
2018-08-17 12:37:16 -0300

    Just changing name to avoid colisions. As it does not manage private keys and addresses on nodes, Helper is more appropriate as it just format addresses.

---
+++ src/main/java/io/smilo/commons/ledger/AddressHelper.java
@@ -0,0 +1,11 @@
+package io.smilo.commons.ledger;
+
+
+import org.apache.log4j.Logger;
+
+public class AddressHelper {
+
+    private static final Logger LOGGER = Logger.getLogger(AddressHelper.class);
+
+}
+
