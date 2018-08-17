d9bb1f9216b78d13ae426fdfb61c247ea47225c5
Thomas Modeneis <thomas.modeneis@gmail.com>
2018-08-17 16:31:44 +0200

    #SMIL-135

---
+++ src/main/java/io/smilo/commons/ledger/AddressManager.java
@@ -0,0 +1,11 @@
+package io.smilo.commons.ledger;
+
+
+import org.apache.log4j.Logger;
+
+public class AddressManager {
+
+    private static final Logger LOGGER = Logger.getLogger(AddressManager.class);
+
+}
+
